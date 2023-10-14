cec1b3bfb3a1ec84e0ff026b19c2ddca870a64fc
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2023-10-13 22:07:41 -0300

    Comments

--- src/main/java/com/paulohenriquegrancieri/workshopmongo/domain/Post.java
+++ src/main/java/com/paulohenriquegrancieri/workshopmongo/domain/Post.java
@@ -1,6 +1,7 @@
 package com.paulohenriquegrancieri.workshopmongo.domain;
 
 import com.paulohenriquegrancieri.workshopmongo.dto.AuthorDTO;
+import com.paulohenriquegrancieri.workshopmongo.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,4 +25,12 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(Date date, String title, String body, AuthorDTO author) {
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
