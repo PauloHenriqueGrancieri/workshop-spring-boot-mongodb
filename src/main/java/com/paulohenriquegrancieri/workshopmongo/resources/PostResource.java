d2982de01698d5b89beee8519e7b1e3f69cbc902
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2023-10-16 20:44:40 -0300

    Consulta por title

--- src/main/java/com/paulohenriquegrancieri/workshopmongo/resources/PostResource.java
+++ src/main/java/com/paulohenriquegrancieri/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.paulohenriquegrancieri.workshopmongo.resources;
 
 import com.paulohenriquegrancieri.workshopmongo.domain.Post;
+import com.paulohenriquegrancieri.workshopmongo.resources.util.URL;
 import com.paulohenriquegrancieri.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -19,5 +19,11 @@ public class PostResource {
     @GetMapping("/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
         return ResponseEntity.ok().body(postService.findById(id));
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        return ResponseEntity.ok().body(postService.findByTitle(text));
     }
 }
--- src/main/java/com/paulohenriquegrancieri/workshopmongo/services/PostService.java
+++ src/main/java/com/paulohenriquegrancieri/workshopmongo/services/PostService.java
@@ -20,6 +20,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return postRepository.findByTitleContaining(text);
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
